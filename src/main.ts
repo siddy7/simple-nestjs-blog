971d73a076e0d39ef88464ec7186539f82b613b8
Siddharthan <siddharthan.ss@superops.ai>
2022-02-23 22:10:37 +0530

    heroku deployment changes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 3000 , () => {
-    console.log("Nest server listening on port %d", this.address().port);
+    console.log("Nest server started listening");
   });
 }
 bootstrap();
