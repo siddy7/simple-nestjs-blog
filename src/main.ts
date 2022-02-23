64fe5b094efa1832113acf7bac74bee162538d6c
Siddharthan <siddharthan.ss@superops.ai>
2022-02-23 21:23:08 +0530

    heroku deployment changes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000 , () => {
-    console.log("Express server listening on port %d in %s mode", this.address().port, process.env.PORT);
+    console.log("Nest server listening on port %d", this.address().port);
   });
 }
 bootstrap();
